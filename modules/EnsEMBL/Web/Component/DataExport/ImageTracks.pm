1a095e24cb04a130d33a0e6ee0fa23ace38fcba0
Anne Lyle <annelyle@ebi.ac.uk>
2016-04-21 13:16:34 +0100

    Added form elements for sequence export

--- modules/EnsEMBL/Web/Component/DataExport/ImageTracks.pm
+++ modules/EnsEMBL/Web/Component/DataExport/ImageTracks.pm
@@ -38,6 +38,11 @@ sub content {
 
   my $form = $self->new_form({'id' => 'export', 'action' => $hub->url({'action' => 'SelectInterface',  'function' => '', '__clear' => 1}), 'method' => 'post'});
 
+  ## We should encourage users to download zipped data if the region is large
+  my $location = $hub->param('r');
+  my ($region, $start, $end) = split(':|-', $location);
+  my $should_zip = $end - $start > '500000' ? 1 : 0;
+
   ## First, the radio buttons for selection data type
   my $datatype_fieldset = $form->add_fieldset;
   my $datatypes = [
@@ -53,14 +58,64 @@ sub content {
                 });
 
   ## Sequence sub-form
+  my $seq_fieldset = $form->add_fieldset({'legend' => 'Download sequence (FASTA)'});
+
+  $seq_fieldset->add_field({
+                            'label' => 'Coordinates',
+                            'type'  => 'NoEdit',
+                            'name'  => 'region',
+                            'value' => $hub->param('r'),
+                          });
+
+  $seq_fieldset->add_field({
+                            'label' => "5' flank",
+                            'type'  => 'Int',
+                            'name'  => 'flank5',
+                            'value' => '0',
+                          });
+
+  $seq_fieldset->add_field({
+                            'label' => "3' flank",
+                            'type'  => 'Int',
+                            'name'  => 'flank3',
+                            'value' => '0',
+                          });
+
+  my $masking = [
+                  {'value' => 'none', 'label' => 'None'},
+                  {'value' => 'soft', 'label' => 'Repeat masked (soft)'},
+                  {'value' => 'hard', 'label' => 'Repeat masked (hard)'},
+                ];
+  $seq_fieldset->add_field({
+                            'label'   => 'Masking',
+                            'type'    => 'Dropdown',
+                            'values'  => $masking,
+                            'value'   => 'none',
+                          });
+
+  $seq_fieldset->add_field({
+                            'label'     => 'File name',
+                            'type'      => 'String',
+                            'value'     => $self->default_file_name,
+                            'shortnote' => '.fa',
+                          });
+
+  $seq_fieldset->add_field({
+                            'label'     => 'Compressed (gzip)',
+                            'type'      => 'Checkbox',
+                            'value'     => 1,
+                            'selected'  => $should_zip ? 'selected' : '',
+                            'shortnote' => 'We recommend zipping your file if the region is large',
+                          });
 
   ## Features sub-form
+  my $feats_fieldset = $form->add_fieldset({'legend' => 'Download features'});
 
   ## Big data sub-form
   my $bigdata_fieldset = $form->add_fieldset;
 
-  my $ftp_base = sprintf '%s/release-%s', $self->hub->species_defs->ENSEMBL_FTP_URL, $self->hub->species_defs->ENSEMBL_VERSION;
-  my $species = lc($self->hub->species_defs->SPECIES_URL);
+  my $ftp_base = sprintf '%s/release-%s', $hub->species_defs->ENSEMBL_FTP_URL, $hub->species_defs->ENSEMBL_VERSION;
+  my $species = lc($hub->species_defs->SPECIES_URL);
   my $text = sprintf '<h2>Large datasets</h2>
 <p>For <b>whole chromosome sequences</b> and <b>complete feature sets</b>, visit our <a href="%s">FTP site</a> for:</p>
 <ul>
@@ -77,7 +132,7 @@ $ftp_base, $species,
 $ftp_base, $species,
 ;
 
-  if ($self->hub->species_defs->ENSEMBL_MART_ENABLED) {
+  if ($hub->species_defs->ENSEMBL_MART_ENABLED) {
     $text .= '<p>For <b>custom datasets in CSV or Excel format</b>, try <a href="/biomart/martview">BioMart</a>.</p>';
   }
 
